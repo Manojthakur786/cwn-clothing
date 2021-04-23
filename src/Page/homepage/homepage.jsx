3a295abb8abdd117eaa4fb93fc83e2e7e296d108
Manojthakur786 <manojthakur123450@gmail.com>
2021-04-24 00:19:03 +0530

    complete menu-item and homepage and styling of menu-item

---
+++ src/Page/homepage/homepage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './homepage.scss';
+import Directory from '../../Component/directory/directory.component';
+const HomePage=()=>(
+    <div className="homepage">
+    <Directory/>
+    </div>
+);
+export default HomePage;
